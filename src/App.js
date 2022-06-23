31e8fa1561e4a1c756b82dbce17924bc009f3305
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-06-22 19:56:49 -0700

    complete counter example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./state/counter/counterSlice";
+
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Counter: {count}</h1>
+
+      <button onClick={() => dispatch(increment())}>Increase</button>
+      <button onClick={() => dispatch(decrement())}>Decrease</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
     </div>
   );
 }
